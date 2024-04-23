c9bc9aae5e83e61115a82f55d5968ffb1b27dca5
SaurusSaurusSaurus <108139526+SaurusSaurusSaurus@users.noreply.github.com>
2024-04-23 18:32:46 -0500

    image -slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Accordian from './components/accordian'
+import ImageSlider from './components/image-slider'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 
@@ -13,7 +14,9 @@ function App() {
         {/* Ramdom Color component*/ }
         {/* <RandomColor/> */}
         {/* Star Rating Component*/ }
-        <StarRating/>
+        {/* <StarRating/> */}
+        {/* Image Slider Component*/ }
+        <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'} />
       </div>
     </>
   )
