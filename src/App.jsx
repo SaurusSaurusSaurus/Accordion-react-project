4fbd3c575f051acc01a03cf3b1143a741fca9461
SaurusSaurusSaurus <108139526+SaurusSaurusSaurus@users.noreply.github.com>
2024-04-25 17:30:26 -0500

    Load More Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import './App.css'
-import Accordian from './components/accordian'
-import ImageSlider from './components/image-slider'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-button";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
-
   return (
     <>
       <div className="root">
-        {/* Accordian component*/ }
+        {/* Accordian component*/}
         {/* <Accordian/> */}
-        {/* Ramdom Color component*/ }
+        {/* Ramdom Color component*/}
         {/* <RandomColor/> */}
-        {/* Star Rating Component*/ }
+        {/* Star Rating Component*/}
         {/* <StarRating/> */}
-        {/* Image Slider Component*/ }
-        <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'} />
+        {/* Image Slider Component*/}
+        {/* <ImageSlider
+          url={"https://picsum.photos/v2/list"}
+          limit={"10"}
+          page={"1"}
+        /> */}
+        {/* Load More Products Component*/}
+        <LoadMoreData/>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
